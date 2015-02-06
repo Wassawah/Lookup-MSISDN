62f4494e1350e8aa9b738a8abf74d031e3e2bd2c
test <test@gmail.com>
2015-02-06 03:29:08 +0100

    True repair for composer install (via puppet). Night :D

--- puppet/modules/composer/manifests/init.pp
+++ puppet/modules/composer/manifests/init.pp
@@ -5,14 +5,14 @@ class composer {
 	}
 
 	exec { 'composer_install':
-		command => 'curl -sS https://getcomposer.org/installer | php && sudo mv composer.phar /usr/local/bin/composer',
+		command => 'curl -sS https://getcomposer.org/installer | php && sudo mv composer.phar usr/local/bin/composer',
 		path    => '/usr/bin:/usr/sbin',
 		require => Package['curl'],
 	}
 
 	exec { 'composer_update':
-		command => 'composer install',
-		cwd    => "/var/www/html/",
+		command => 'composer install -d /var/www/html/',
+		timeout => 0,
 		require => [ Package['curl'], Exec['existHTTP'] ]
 	}
 }

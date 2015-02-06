40ea371856c50efa94cd77f45bb687625c91c9e3
test <test@gmail.com>
2015-02-06 10:20:00 +0100

    Error repair!

--- puppet/modules/composer/manifests/init.pp
+++ puppet/modules/composer/manifests/init.pp
@@ -5,7 +5,7 @@ class composer {
 	}
 
 	exec { 'composer_install':
-		command => 'curl -sS https://getcomposer.org/installer | php && sudo mv composer.phar usr/local/bin/composer',
+		command => 'curl -sS https://getcomposer.org/installer | php && sudo mv composer.phar /usr/local/bin/composer',
 		path    => '/usr/bin:/usr/sbin',
 		require => Package['curl'],
 	}
